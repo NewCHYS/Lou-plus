8ba3f49080a05f039d9fed5a78f0d5f07833d98f
NewCHYS <270620226@qq.com>
2018-02-28 17:30:34 +0800

    challenge15

---
+++ spider15.py
@@ -0,0 +1,22 @@
+# -*- conding:utf-8 -*-
+
+import scrapy
+
+class Spider(scrapy.Spider):
+    name = 'shiyanlou-courses'
+
+    def start_requests(self):
+        url_tmpl = 'https://github.com/shiyanlou?page={}&tab=repositories'
+
+        urls = (url_tmpl.format(i) for i in range(1, 5))
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for course in response.xpath('//*[@id="user-repositories-list"]/ul/li'):
+            yield{
+                    'name':course.xpath('.//div[1]/h3/a/text()').re_first('(\S+)'),
+                    'update_time': course.xpath('.//relative-time/@datetime').extract_first()
+                    }
+
