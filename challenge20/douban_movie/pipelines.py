5f57a6711d3e733ac70829ee0b774edab2128ca6
2 <2@2.2>
2018-03-14 12:26:45 +0800

    challenge20

--- challenge20/douban_movie/pipelines.py
+++ challenge20/douban_movie/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import redis
+import json
 
 
 class DoubanMoviePipeline(object):
+    def open_spider(self, spider):
+        self.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
     def process_item(self, item, spider):
+        url = item['url']
+        name = item['name']
+        summary = item['summary']
+        score = item['score']
+        items = json.dumps({'url':url, 'name':name, 'summary':summary, 'score':score})
+        self.redis.lpush('douban_movie:items', items)
         return item
