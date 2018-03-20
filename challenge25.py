3ffbcfe09f9b65c7eabd60ac1f6b12b7aaf0c54e
NewCHYS <270620226@qq.com>
2018-03-20 19:35:32 +0800

    challenge25

---
+++ challenge25.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+
+
+def co2():
+
+    # 1
+    df_climate = pd.read_excel("ClimateChange.xlsx", sheetname='Data')
+    Country = pd.read_excel("ClimateChange.xlsx", sheetname='Country')
+    Data2 = df_climate[df_climate['Series code']=='EN.ATM.CO2E.KT']
+    # print(Data2)
+
+    # 2
+    Data2 = Data2.drop(['Country code','Series code','Series name','SCALE','Decimals'], axis=1)
+    # print(Data2)
+    r = Data2.loc[1527, 1990]
+    Data2 = Data2.replace(r, np.nan)
+    Data2.set_index('Country name', inplace=True)
+    Data2 = Data2.fillna(method='ffill', axis=1).fillna(method='bfill', axis=1)
+    Data2 = Data2.dropna()
+    Data3 = Data2.sum(axis=1)
+    # print(Data3)
+
+    # 3
+    Country = Country.loc[:, ['Country name', 'Income group']]
+    Country.set_index('Country name', inplace=True)
+    # print(Country)
+    Data4 = pd.concat([Country, Data3], axis=1, join='inner')
+    Data4.rename(columns={0:'sum'}, inplace=True)
+    Data4 = Data4.reset_index()
+    # print(Data4)
+    
+    # 4
+    index = ['High income: OECD', 'High income: nonOECD', 'Low income', 'Lower middle income', 'Upper middle income']
+    columns = ['Income group', 'Sum emissions', 'Highest emission country', 'Highest emissions', 'Lowest emission country', 'Lowest emissions']
+    Dataframe = DataFrame(columns=columns)
+    Dataframe['Income group'] = index
+    Dataframe.set_index('Income group', inplace=True)
+    # print(Dataframe)
+
+    # 5
+    Data_sum = Data4.groupby(Data4['Income group']).sum()
+    # print(Data_sum)
+    for i in Data_sum.index:
+        # print(Data_sum.loc[i]['sum'])
+        Dataframe.loc[i]['Sum emissions'] = Data_sum.loc[i]['sum']
+    # print(Dataframe)
+    Data_max = Data4.groupby(Data4['Income group']).max()
+    # print(Data_max)
+    for i in Data_max.index:
+        Dataframe.loc[i]['Highest emission country'] = Data_max.loc[i]['Country name']
+        Dataframe.loc[i]['Highest emissions'] = Data_max.loc[i]['sum']
+    # print(Dataframe)
+    Data_min = Data4.groupby(Data4['Income group']).min()
+    # print(Data_min)
+    for i in Data_min.index:
+        Dataframe.loc[i]['Lowest emission country'] = Data_min.loc[i]['Country name']
+        Dataframe.loc[i]['Lowest emissions'] = Data_min.loc[i]['sum']
+    # print(Dataframe)
+    return Dataframe
+
+
+if __name__ == '__main__':
+    print(co2())
+
