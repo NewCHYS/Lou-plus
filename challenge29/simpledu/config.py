cc7abd2d198746215f5cdc87b720adb2c628973e
CHYS <CHYS@localhost.localdomain>
2018-03-28 08:40:52 +0800

    challenge29

---
+++ challenge29/simpledu/config.py
@@ -0,0 +1,27 @@
+class BaseConfig(object):
+
+	SECRET_KEY = '000000'
+
+
+class DevelopmentConfig(BaseConfig):
+
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:67119299Mj!@localhost:3306/simpledu?charset=utf8'
+
+
+class ProductionConfig(BaseConfig):
+
+	pass
+
+
+class TestingConfig(BaseConfig):
+
+	pass
+
+
+configs = {
+	'development': DevelopmentConfig,
+	'production': ProductionConfig,
+	'testing': TestingConfig
+}
+
---
+++ challenge29/simpledu/handlers/user.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+from simpledu.models import User
+
+
+user = Blueprint('user', __name__, url_prefix='/user/<username>')
+
+@user.route('/')
+def index(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	return render_template('user.html', user=user)
+
