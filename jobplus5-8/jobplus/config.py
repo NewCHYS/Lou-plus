e77c29670515abd5862045fa2136ad23dd0a4389
NewCHYS <270620226@qq.com>
2018-04-18 20:55:41 +0800

    Update config.py

--- jobplus5-8/jobplus/config.py
+++ jobplus5-8/jobplus/config.py
@@ -6,7 +6,7 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:67119299Mj!@localhost:3306/jobplus?charset=utf8"
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8"
 
 
 class ProductionConfig(BaseConfig):
